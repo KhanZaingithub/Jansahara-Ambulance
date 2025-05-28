919ef1ccdb5400cf09ce43aa689d2daa6cfcb813
Zainul khan <zainulkhan032@gmail.com>
2025-05-28 11:14:05 +0530

    header and footer added

--- include/discount-button.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="css/discount-button.css">
-
-<div class="discount-btn">
-    <a href="your-discount-page.php" target="_blank">
-        <img src="images/discount-offer-3.png" alt="Special Discount" class="discount-img">
-    </a>
-</div>
