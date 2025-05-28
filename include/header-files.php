eae7563edb1aa32cd48c6b04bca8337331f4027f
Zainul khan <zainulkhan032@gmail.com>
2025-05-28 10:18:25 +0530

    try header

--- include/header-files.php
+++ include/header-files.php
@@ -1,9 +1,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-		<![endif]-->
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400,700%3Alatin%7CMontserrat%3A700%3Alatin">
 	<link rel="stylesheet" href="css\style.css">
 	<link rel="stylesheet" href="css\prettyPhoto.css">
