4afd501215cbc29c2a9ea00889911eaf75d0929f
Zainul khan <zainulkhan032@gmail.com>
2025-05-30 17:10:45 +0530

    sameer demo

---
+++ js/about.js
@@ -0,0 +1,14 @@
+(function ($) {
+  "use strict";
+  /*=========================================
+    =            Preloader active            =
+    ===========================================*/
+
+  jQuery(window).on("load", function () {
+    jQuery(".preloader-activate").removeClass("preloader-active");
+
+    setTimeout(function () {
+      jQuery(".open_tm_preloader").addClass("loaded");
+    }, 500);
+  });
+})(jQuery);
